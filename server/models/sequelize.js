e30ec9964b367317a4844cb2d1a5338d36eda29c
Vlad <vlad.constantinescu65@yahoo.com>
2023-02-10 17:40:33 +0200

    Modified db engine to postgresql

--- server/models/sequelize.js
+++ server/models/sequelize.js
@@ -1,11 +1,13 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "./database.db",
-  define: {
-    timestamps: false,
-  },
-});
+const sequelize = new Sequelize(
+  "postgres://pjbezxfc:E5KxEtQBK1ijb8ygK2WQeFQWGhfggLiA@kandula.db.elephantsql.com/pjbezxfc",
+  {
+    dialect: "postgres",
+    define: {
+      timestamps: false,
+    },
+  }
+);
 
 module.exports = sequelize;
